96b61eef7781e30ee37750048ee49b2b6a10209b
Tomasz Cielecki <tci@trackman.com>
2020-08-11 08:58:37 +0200

    Add failing sample

---
+++ core/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Abstractions;
+
+namespace core
+{
+    public class Class1
+    {
+        public string Herp()
+        {
+            var file = new FileSystem();
+            using var stream = file.File.OpenRead("herp");
+            using var reader = new StreamReader(stream);
+            var content = reader.ReadToEnd();
+            return content;
+        }
+    }
+}
---
+++ sysioabsfail/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using core;
+using Foundation;
+using System;
+using UIKit;
+
+namespace sysioabsfail
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            var content = new Class1().Herp();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
